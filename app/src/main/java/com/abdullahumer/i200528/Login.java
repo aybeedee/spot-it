fc64b0c9fc398cff94e947de3532ca97f10567cc
aybeedee <abdullahumer575@gmail.com>
2023-09-13 19:44:04 +0500

    added forgot password page and respective navigations

--- app/src/main/java/com/abdullahumer/i200528/Login.java
+++ app/src/main/java/com/abdullahumer/i200528/Login.java
@@ -5,18 +5,25 @@ import androidx.appcompat.app.AppCompatActivity;
 import android.content.Intent;
 import android.os.Bundle;
 import android.view.View;
+import android.widget.TextView;
 
 public class Login extends AppCompatActivity {
+
+    TextView forgot_password;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_login);
-    }
 
-    public void navigateForgotPassword(View view) {
-        Intent intent = new Intent(Login.this,
-                ForgotPassword.class);
-        startActivity(intent);
+        forgot_password = findViewById(R.id.text_forgot_password);
+
+        forgot_password.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Intent intent = new Intent(Login.this, ForgotPassword.class);
+                startActivity(intent);
+            }
+        });
     }
 }
