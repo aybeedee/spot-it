41e9ba51c1885e3fb3bd9db272f49aae0d128f62
aybeedee <abdullahumer575@gmail.com>
2023-09-13 11:20:49 +0500

    added splash screen

---
+++ app/src/main/java/com/abdullahumer/i200528/SplashScreen.java
@@ -0,0 +1,29 @@
+package com.abdullahumer.i200528;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.content.Intent;
+import android.os.Build;
+import android.os.Bundle;
+import android.view.Window;
+import android.view.WindowManager;
+
+import java.util.Timer;
+import java.util.TimerTask;
+
+public class SplashScreen extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_splash_screen);
+
+        new Timer().schedule(new TimerTask() {
+            @Override
+            public void run() {
+                startActivity(new Intent(getApplicationContext(), Login.class));
+            }
+        }, 3000);
+    }
+}
