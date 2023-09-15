c9d2b64d98006ecb2e6a3a3edec7d98544a2b88d
aybeedee <abdullahumer575@gmail.com>
2023-09-15 12:27:46 +0500

    added navigation to and from search results

--- app/src/main/java/com/abdullahumer/i200528/SearchFragment.java
+++ app/src/main/java/com/abdullahumer/i200528/SearchFragment.java
@@ -1,5 +1,6 @@
 package com.abdullahumer.i200528;
 
+import android.content.Intent;
 import android.os.Bundle;
 
 import androidx.fragment.app.Fragment;
@@ -8,6 +9,7 @@ import android.util.Log;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.LinearLayout;
 
 /**
  * A simple {@link Fragment} subclass.
@@ -24,6 +26,8 @@ public class SearchFragment extends Fragment {
     // TODO: Rename and change types of parameters
     private String mParam1;
     private String mParam2;
+
+    LinearLayout search_item;
 
     public SearchFragment() {
         // Required empty public constructor
@@ -60,7 +64,20 @@ public class SearchFragment extends Fragment {
     @Override
     public View onCreateView(LayoutInflater inflater, ViewGroup container,
                              Bundle savedInstanceState) {
+
         // Inflate the layout for this fragment
-        return inflater.inflate(R.layout.fragment_search, container, false);
+        View view = inflater.inflate(R.layout.fragment_search, container, false);
+
+        search_item = view.findViewById(R.id.linear_search_item);
+
+        search_item.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Intent intent = new Intent(getActivity(), SearchResults.class);
+                startActivity(intent);
+            }
+        });
+
+        return view;
     }
 }
--- app/src/main/java/com/abdullahumer/i200528/SearchResults.java
+++ app/src/main/java/com/abdullahumer/i200528/SearchResults.java
@@ -7,21 +7,21 @@ import android.os.Bundle;
 import android.view.View;
 import android.widget.ImageView;
 
-public class EmailVerification extends AppCompatActivity {
+public class SearchResults extends AppCompatActivity {
 
-    ImageView back_email;
+    ImageView back_search;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_email_verification);
+        setContentView(R.layout.activity_search_results);
 
-        back_email = findViewById(R.id.image_back_email);
+        back_search = findViewById(R.id.image_back_search);
 
-        back_email.setOnClickListener(new View.OnClickListener() {
+        back_search.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
-                Intent intent = new Intent(EmailVerification.this, SignUp.class);
+                Intent intent = new Intent(SearchResults.this, MainActivity.class);
                 startActivity(intent);
             }
         });
