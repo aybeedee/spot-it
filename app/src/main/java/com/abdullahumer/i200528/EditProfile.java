81ef31fed0b1818c60d1607fb8c03aa11a788f27
aybeedee <abdullahumer575@gmail.com>
2023-10-17 14:10:53 +0500

    upload profile and cover photo and edit profile

--- app/src/main/java/com/abdullahumer/i200528/EditProfile.java
+++ app/src/main/java/com/abdullahumer/i200528/EditProfile.java
@@ -1,17 +1,45 @@
 package com.abdullahumer.i200528;
 
+import androidx.annotation.NonNull;
+import androidx.annotation.Nullable;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.content.Intent;
+import android.net.Uri;
 import android.os.Bundle;
+import android.util.Log;
 import android.view.View;
 import android.widget.ArrayAdapter;
+import android.widget.EditText;
 import android.widget.ImageView;
+import android.widget.LinearLayout;
 import android.widget.Spinner;
+import android.widget.TextView;
+import android.widget.Toast;
+
+import com.google.android.gms.tasks.OnCompleteListener;
+import com.google.android.gms.tasks.OnFailureListener;
+import com.google.android.gms.tasks.OnSuccessListener;
+import com.google.android.gms.tasks.Task;
+import com.google.firebase.auth.FirebaseAuth;
+import com.google.firebase.database.DatabaseReference;
+import com.google.firebase.database.FirebaseDatabase;
+import com.google.firebase.storage.FirebaseStorage;
+import com.google.firebase.storage.StorageReference;
+import com.google.firebase.storage.UploadTask;
+
+import java.util.Calendar;
 
 public class EditProfile extends AppCompatActivity {
 
     ImageView back_edit;
+    LinearLayout changeDp, changeCover;
+    TextView save;
+    EditText name, contact, email;
+
+    String userId, dpUrl, coverUrl;
+
+    FirebaseAuth mAuth;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -30,6 +58,60 @@ public class EditProfile extends AppCompatActivity {
         citySpinner.setAdapter(citySpinnerAdapter);
 
         back_edit = findViewById(R.id.image_back_edit);
+        changeDp = findViewById(R.id.changeDp);
+        changeCover = findViewById(R.id.changeCover);
+        name = findViewById(R.id.name);
+        contact = findViewById(R.id.contact);
+        email = findViewById(R.id.email);
+
+        save = findViewById(R.id.save);
+
+        //initiate
+        mAuth = FirebaseAuth.getInstance();
+
+        userId = mAuth.getUid().toString();
+
+
+
+        changeDp.setOnClickListener(new View.OnClickListener() {
+
+            @Override
+            public void onClick(View v) {
+
+                Intent intent = new Intent();
+                intent.setType("image/*");
+                intent.setAction(Intent.ACTION_GET_CONTENT);
+                startActivityForResult(intent, 100);
+            }
+        });
+
+        changeCover.setOnClickListener(new View.OnClickListener() {
+
+            @Override
+            public void onClick(View v) {
+
+                Intent intent = new Intent();
+                intent.setType("image/*");
+                intent.setAction(Intent.ACTION_GET_CONTENT);
+                startActivityForResult(intent, 101);
+            }
+        });
+
+        save.setOnClickListener(new View.OnClickListener() {
+
+            @Override
+            public void onClick(View v) {
+
+                DatabaseReference mDatabase = FirebaseDatabase.getInstance().getReference();
+
+                mDatabase.child("users").child(userId).child("profilePhotoUrl").setValue(dpUrl);
+                mDatabase.child("users").child(userId).child("coverPhotoUrl").setValue(coverUrl);
+                mDatabase.child("users").child(userId).child("fullName").setValue(name.getText().toString());
+                mDatabase.child("users").child(userId).child("contactNumber").setValue(contact.getText().toString());
+                mDatabase.child("users").child(userId).child("country").setValue(countrySpinner.getSelectedItem().toString());
+                mDatabase.child("users").child(userId).child("city").setValue(citySpinner.getSelectedItem().toString());
+            }
+        });
 
         back_edit.setOnClickListener(new View.OnClickListener() {
 
@@ -38,8 +120,73 @@ public class EditProfile extends AppCompatActivity {
 
                 Intent intent = new Intent(EditProfile.this, MainActivity.class);
                 startActivity(intent);
-
             }
         });
+    }
+
+    @Override
+    protected void onActivityResult(int requestCode, int resultCode, @Nullable Intent data) {
+        super.onActivityResult(requestCode, resultCode, data);
+        if (requestCode == 100 && resultCode == RESULT_OK) {
+            Uri image = data.getData();
+            FirebaseStorage storage = FirebaseStorage.getInstance();
+            StorageReference storageRef = storage.getReference().child(mAuth.getUid()+ Calendar.getInstance().getTimeInMillis()+"-dp.png");
+            storageRef.putFile(image)
+                    .addOnSuccessListener(new OnSuccessListener<UploadTask.TaskSnapshot>() {
+
+                        @Override
+                        public void onSuccess(UploadTask.TaskSnapshot taskSnapshot) {
+
+                            Task<Uri> task = taskSnapshot.getStorage().getDownloadUrl();
+                            task.addOnSuccessListener(new OnSuccessListener<Uri>() {
+                                @Override
+                                public void onSuccess(Uri uri) {
+                                    dpUrl = uri.toString();
+                                    Toast.makeText(EditProfile.this, dpUrl, Toast.LENGTH_LONG).show();
+                                }
+                            });
+                        }
+                    })
+                    .addOnFailureListener(new OnFailureListener() {
+                        @Override
+                        public void onFailure(@NonNull Exception e) {
+                            Toast.makeText(EditProfile.this, e.getMessage().toString(), Toast.LENGTH_LONG).show();
+                        }
+                    });
+        }
+
+        else if (requestCode == 101 && resultCode == RESULT_OK) {
+            Uri image = data.getData();
+            FirebaseStorage storage = FirebaseStorage.getInstance();
+            StorageReference storageRef = storage.getReference().child(mAuth.getUid()+ Calendar.getInstance().getTimeInMillis()+"-coverphoto.png");
+            storageRef.putFile(image)
+                    .addOnSuccessListener(new OnSuccessListener<UploadTask.TaskSnapshot>() {
+
+                        @Override
+                        public void onSuccess(UploadTask.TaskSnapshot taskSnapshot) {
+
+                            Task<Uri> task = taskSnapshot.getStorage().getDownloadUrl();
+                            task.addOnSuccessListener(new OnSuccessListener<Uri>() {
+                                @Override
+                                public void onSuccess(Uri uri) {
+                                    coverUrl = uri.toString();
+                                    Toast.makeText(EditProfile.this, coverUrl, Toast.LENGTH_LONG).show();
+                                }
+                            });
+                        }
+                    })
+                    .addOnFailureListener(new OnFailureListener() {
+                        @Override
+                        public void onFailure(@NonNull Exception e) {
+                            Toast.makeText(EditProfile.this, e.getMessage().toString(), Toast.LENGTH_LONG).show();
+                        }
+                    });
+        }
+    }
+
+    @Override
+    public void onBackPressed() {
+        super.onBackPressed();
+        setResult(RESULT_CANCELED);
     }
 }
