7e6f161e6056e5a1e18833977233c9a88c1339d7
aybeedee <abdullahumer575@gmail.com>
2023-10-22 18:41:00 +0500

    fix imageview scaletype issues

--- app/src/main/java/com/abdullahumer/i200528/ProfileFragment.java
+++ app/src/main/java/com/abdullahumer/i200528/ProfileFragment.java
@@ -3,15 +3,26 @@ package com.abdullahumer.i200528;
 import android.content.Intent;
 import android.os.Bundle;
 
+import androidx.annotation.NonNull;
 import androidx.fragment.app.Fragment;
 
+import android.util.Log;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
 import android.widget.ImageView;
 import android.widget.TextView;
+import android.widget.Toast;
 
+import com.google.android.gms.tasks.OnCompleteListener;
+import com.google.android.gms.tasks.Task;
 import com.google.firebase.auth.FirebaseAuth;
+import com.google.firebase.database.DataSnapshot;
+import com.google.firebase.database.DatabaseError;
+import com.google.firebase.database.DatabaseReference;
+import com.google.firebase.database.FirebaseDatabase;
+import com.google.firebase.database.ValueEventListener;
+import com.squareup.picasso.Picasso;
 
 /**
  * A simple {@link Fragment} subclass.
@@ -29,10 +40,13 @@ public class ProfileFragment extends Fragment {
     private String mParam1;
     private String mParam2;
 
-    ImageView edit_nav;
-    TextView logout;
+    ImageView edit_nav, profilePhoto, coverPhoto;
+    TextView logout, name, city, itemsPosted, itemsRented;
 
     FirebaseAuth mAuth;
+    DatabaseReference mDatabase;
+
+    String userId;
 
     public ProfileFragment() {
         // Required empty public constructor
@@ -74,6 +88,85 @@ public class ProfileFragment extends Fragment {
 
         edit_nav = view.findViewById(R.id.image_edit_nav);
         logout = view.findViewById(R.id.logout);
+        profilePhoto = view.findViewById(R.id.profilePhoto);
+        coverPhoto = view.findViewById(R.id.coverPhoto);
+        name = view.findViewById(R.id.name);
+        city = view.findViewById(R.id.city);
+        itemsPosted = view.findViewById(R.id.itemsPosted);
+        itemsRented = view.findViewById(R.id.itemsRented);
+
+        mAuth = FirebaseAuth.getInstance();
+        userId = mAuth.getUid().toString();
+
+        mDatabase = FirebaseDatabase.getInstance().getReference();
+
+        mDatabase.child("users").child(userId).get().addOnCompleteListener(new OnCompleteListener<DataSnapshot>() {
+
+            @Override
+            public void onComplete(@NonNull Task<DataSnapshot> task) {
+
+                if (task.isSuccessful()) {
+
+                    User userObject = task.getResult().getValue(User.class);
+
+                    Picasso.get().load(userObject.getCoverPhotoUrl()).into(coverPhoto);
+                    Picasso.get().load(userObject.getProfilePhotoUrl()).into(profilePhoto);
+                    name.setText(userObject.getFullName());
+                    city.setText(userObject.getCity());
+
+                    mDatabase.child("userPosts").child(userId).addListenerForSingleValueEvent(new ValueEventListener() {
+
+                        @Override
+                        public void onDataChange(@NonNull DataSnapshot snapshot) {
+
+                            if (snapshot.exists()) {
+
+                                itemsPosted.setText(String.valueOf(snapshot.getChildrenCount()) + " items posted");
+                            }
+
+                            else {
+
+                                itemsPosted.setText("0 items posted");
+                            }
+                        }
+
+                        @Override
+                        public void onCancelled(@NonNull DatabaseError error) {
+
+                            Toast.makeText(getContext(), "Could not fetch user's posted items", Toast.LENGTH_LONG).show();
+                        }
+                    });
+
+                    mDatabase.child("userRents").child(userId).addListenerForSingleValueEvent(new ValueEventListener() {
+
+                        @Override
+                        public void onDataChange(@NonNull DataSnapshot snapshot) {
+
+                            if (snapshot.exists()) {
+
+                                itemsRented.setText(String.valueOf(snapshot.getChildrenCount()) + " items rented");
+                            }
+
+                            else {
+
+                                itemsRented.setText("0 items rented");
+                            }
+                        }
+
+                        @Override
+                        public void onCancelled(@NonNull DatabaseError error) {
+
+                            Toast.makeText(getContext(), "Could not fetch user's rented items", Toast.LENGTH_LONG).show();
+                        }
+                    });
+                }
+
+                else {
+
+                    Log.e("DBErr", "Could not fetch user data", task.getException());
+                }
+            }
+        });
 
         edit_nav.setOnClickListener(new View.OnClickListener() {
 
@@ -89,7 +182,6 @@ public class ProfileFragment extends Fragment {
 
             @Override
             public void onClick(View v) {
-
 
                 mAuth = FirebaseAuth.getInstance();
 
